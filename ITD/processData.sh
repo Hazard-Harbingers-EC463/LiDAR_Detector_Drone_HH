6faa2891807dece4baeac1f53a056fc76c5a6da6
Julia <jhua2@bu.edu>
2023-11-16 12:34:28 -0500

    Fixed parameters for DTM and CHM, fixed ground point classification

--- ITD/processData.sh
+++ ITD/processData.sh
@@ -1,60 +1,54 @@
 #!/bin/bash
 # Must run as administrator
 
-# Get LiDAR data
-echo "Please input file path to LiDAR data: "
+# Parameters for creating models
+CELL_SIZE=5
+XY_UNITS=m
+Z_UNITS=m
+COORD_SYS=0
+ZONE=0
+HORIZ_DATUM=0
+VERT_DATUM=0
+
+# Get LiDAR data: convert to LAS format
+echo ">> Please input file path to LiDAR data: "
 read filePath
 fileExtension="${filePath##*.}"
 if [ -e "$filePath" ]; then
     if [ "$fileExtension" = "XYZ" ]; then 
         ../LAStools/txt2las -i "$filePath" -o "${filePath%.*}.las"
         filePath="${filePath%.*}.las"
-        echo "Converted to las file: $filePath"
+        echo ">> Converted to las file: $filePath"
     elif [ "$fileExtension" = "las" ]; then 
-        echo "Remain as las file: $filePath"
+        echo ">> Remain as las file: $filePath"
     else
-        echo "File has extension $fileExtension. Only XYZ or las extensions allowed."
+        echo ">> File has extension $fileExtension. Only XYZ or las extensions allowed."
         exit 1
     fi
 else
-    echo "File does not exist at $filePath. Make sure you are using forward slashes."
+    echo ">> File does not exist at $filePath. Make sure you are using forward slashes."
     exit 1
 fi
 directory=$(dirname "$filePath")
+origDataFilePath=$filePath
+gndPtFilePath="${directory}/ground_points.las"
+dtmFilePath=${directory}/digital_terrain.dtm
+chmFilePath=${directory}/canopy_height.dtm
 
-# Filtering ground points using lasground_new
-echo "Filtering ground points..."
-groundPtFilePath="${directory}/groundPts.las"
-## Add some parameters for this:
-../LAStools/lasground_new -i "$filePath" -o "${groundPtFilePath}"
-echo "Filtered ground points found here: $groundPtFilePath"
+echo ">> Filtering ground points..."
+GroundFilter $gndPtFilePath $CELL_SIZE $origDataFilePath
+echo ">> Filtered ground points found here: $gndPtFilePath"
+sleep 3
 
-# Generating DTM model using FUSION/LDV: GridSurfaceCreate
-dtmFilePath="${directory}/gnd.dtm"
-## Parameters:
-dtm_cellsize=1
-dtm_xyunits="m"
-dtm_zunits="m"
-dtm_coordsys="1"
-dtm_zone="10"
-dtm_horizdatum="2"
-dtm_vertdatum="2"
-GridSurfaceCreate $dtmFilePath $dtm_cellsize $dtm_xyunits $dtm_zunits $dtm_coordsys $dtm_zone $dtm_horizdatum $dtm_vertdatum $groundPtFilePath
+echo ">> Creating digital terrain model (DTM)"
+GridSurfaceCreate $dtmFilePath $CELL_SIZE $XY_UNITS $Z_UNITS $COORD_SYS $ZONE $HORIZ_DATUM $VERT_DATUM $gndPtFilePath
+echo ">> DTM found here: $dtmFilePath"
+sleep 3
 
-# Height aboveground normalization using FUSION/LDV: ClipData
-heightNormalizedFilePath=${directory}/height_normalized.las
-ClipData /ground:$dtmFilePath $filePath $heightNormalizedFilePath 6188142 1811147 6288542 1883547
+echo ">> Creating canopy height model (CHM)"
+CanopyModel $chmFilePath $CELL_SIZE $XY_UNITS $Z_UNITS $COORD_SYS $ZONE $HORIZ_DATUM $VERT_DATUM $origDataFilePath
+#CanopyModel /ground:$dtmFilePath $chmFilePath $CELL_SIZE $XY_UNITS $Z_UNITS $COORD_SYS $ZONE $HORIZ_DATUM $VERT_DATUM $origDataFilePath
+echo ">> CHM found here: $chmFilePath"
+sleep 3
 
-# Generating CHM using FUSION/LDV: CanopyModel
-canopySurfaceFilePath=${directory}/canopy_surface.dtm
-## Parameters
-chm_cellsize=5
-chm_xyunits="m"
-chm_zunits="m"
-chm_coordsys="1"
-chm_zone="10"
-chm_horizdatum="2"
-chm_vertdatum="2"
-CanopyModel /ground:$dtmFilePath $canopySurfaceFilePath $chm_cellsize $chm_xyunits $chm_zunits $chm_coordsys $chm_zone $chm_horizdatum $chm_vertdatum $heightNormalizedFilePath
-
-sleep 20
+sleep 100
