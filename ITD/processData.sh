fe5473f2606c2b5e7ec4d2571e98bcd2028e39e8
Julia <jhua2@bu.edu>
2024-02-21 21:15:46 -0500

    Fixed CHM and added DSM

--- ITD/processData.sh
+++ ITD/processData.sh
@@ -13,7 +13,6 @@ VERT_DATUM=0
 # Parameter for Tree Segmentation
 MIN_HEIGHT=2
 
-
 # Get LiDAR data: convert to LAS format
 echo ">> Please input file path to LiDAR data: "
 read filePath
@@ -33,29 +32,41 @@ else
     echo ">> File does not exist at $filePath. Make sure you are using forward slashes."
     exit 1
 fi
+
 directory=$(dirname "$filePath")
+outPath="${directory}/outputs"
+mkdir -p $outPath
 origDataFilePath=$filePath
 gndPtFilePath="${directory}/ground_points.las"
-dtmFilePath=${directory}/digital_terrain.dtm
-chmFilePath=${directory}/canopy_height.dtm
-treeSegmFilePath=${directory}/tree_segm.csv
+dtmFilePath=${outPath}/digital_terrain.dtm
+dsmFilePath=${outPath}/digital_surface.dtm
+chmFilePath=${outPath}/canopy_height.dtm
+treeSegmFilePath=${outPath}/tree_segm.csv
 
 echo ">> Filtering ground points..."
 GroundFilter $gndPtFilePath $CELL_SIZE $origDataFilePath
 echo ">> Filtered ground points found here: $gndPtFilePath"
 sleep 3
 
+# ========= DTM =========
 echo ">> Creating digital terrain model (DTM)"
 GridSurfaceCreate $dtmFilePath $CELL_SIZE $XY_UNITS $Z_UNITS $COORD_SYS $ZONE $HORIZ_DATUM $VERT_DATUM $gndPtFilePath
 echo ">> DTM found here: $dtmFilePath"
 sleep 3
 
+# ========= DSM/CSM =========
+echo ">> Creating digital surface model (DSM)/canopy surface model (CSM)"
+CanopyModel $dsmFilePath $CELL_SIZE $XY_UNITS $Z_UNITS $COORD_SYS $ZONE $HORIZ_DATUM $VERT_DATUM $origDataFilePath
+echo ">> DSM found here: $dsmFilePath"
+sleep 3
+
+# ========= CHM =========
 echo ">> Creating canopy height model (CHM)"
-CanopyModel $chmFilePath $CELL_SIZE $XY_UNITS $Z_UNITS $COORD_SYS $ZONE $HORIZ_DATUM $VERT_DATUM $origDataFilePath
-#CanopyModel /ground:$dtmFilePath $chmFilePath $CELL_SIZE $XY_UNITS $Z_UNITS $COORD_SYS $ZONE $HORIZ_DATUM $VERT_DATUM $origDataFilePath
+CanopyModel /ground:$dtmFilePath $chmFilePath $CELL_SIZE $XY_UNITS $Z_UNITS $COORD_SYS $ZONE $HORIZ_DATUM $VERT_DATUM $origDataFilePath
 echo ">> CHM found here: $chmFilePath"
 sleep 3
 
+# ========= Tree Segmentation =========
 echo ">> Detecting trees"
 TreeSeg $chmFilePath $MIN_HEIGHT $treeSegmFilePath
 echo ">> Tree detection results found here: $treeSegmFilePath"
