f9edd354f5f53d655f3dcd1c04428b2593ff0bc1
Julia <jhua2@bu.edu>
2024-02-26 00:23:32 -0500

    Added canopy cover and vegetation density models

--- Models/processData.sh
+++ Models/processData.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Must run as administrator
 
-# Parameters for creating DTM and CHM
+# General Parameters
 CELL_SIZE=5
 XY_UNITS=m
 Z_UNITS=m
@@ -9,9 +9,6 @@ COORD_SYS=0
 ZONE=0
 HORIZ_DATUM=0
 VERT_DATUM=0
-
-# Parameter for Tree Segmentation
-MIN_HEIGHT=2
 
 # Get LiDAR data: convert to LAS format
 echo ">> Please input file path to LiDAR data: "
@@ -42,6 +39,8 @@ dtmFilePath=${outPath}/digital_terrain.dtm
 dsmFilePath=${outPath}/digital_surface.dtm
 chmFilePath=${outPath}/canopy_height.dtm
 treeSegmFilePath=${outPath}/tree_segm.csv
+canopyCoverFilePath=${outPath}/cover.dtm
+densityFilePath=${outPath}/density
 
 echo ">> Filtering ground points..."
 GroundFilter $gndPtFilePath $CELL_SIZE $origDataFilePath
@@ -68,8 +67,31 @@ sleep 3
 
 # ========= Tree Segmentation =========
 echo ">> Detecting trees"
+MIN_HEIGHT=2
 TreeSeg $chmFilePath $MIN_HEIGHT $treeSegmFilePath
 echo ">> Tree detection results found here: $treeSegmFilePath"
+sleep 3
+
+# ========= Canopy Cover =========
+echo ">> Canopy cover"
+HEIGHT_BRK=8
+OP_CELL_SZ=50
+Cover $dtmFilePath $canopyCoverFilePath $HEIGHT_BRK $OP_CELL_SZ $XY_UNITS $Z_UNITS $COORD_SYS $ZONE $HORIZ_DATUM $VERT_DATUM $origDataFilePath
+echo ">> Canopy cover model found here: $canopyCoverFilePath"
+sleep 3
+
+# ========= Vegetation Density for Various Height Strata =========
+echo ">> Vegetation density"
+BTM_HT=0
+TOP_HT=15
+STEP=3
+for ((i=BTM_HT; i<=TOP_HT; i+=STEP)); do
+    UPPER_HT=$((i+STEP))
+    densityFile=${densityFilePath}_${i}_${UPPER_HT}.dtm
+
+    ./density.bat /upper:$UPPER_HT $dtmFilePath $densityFile $i $OP_CELL_SZ $XY_UNITS $Z_UNITS $COORD_SYS $ZONE $HORIZ_DATUM $VERT_DATUM $origDataFilePath
+    echo ">> Density in the range of $i and $UPPER_HT above the ground can be found here: $densityFile"
+done
 sleep 3
 
 sleep 100
